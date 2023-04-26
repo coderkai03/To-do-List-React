3e0f6f86247ca05835ceedbe05f9602767fbf103
Rian Corcino <riancorci@gmail.com>
2023-04-26 12:34:37 -0700

    created navbar

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>The Rian++ Blog</h1>
+            <div className="links">
+                <a href="/">Hoom</a>
+                <a href="/create">New Bloog</a>
+
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
