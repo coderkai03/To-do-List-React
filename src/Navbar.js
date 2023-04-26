c0230ee3fa04a920b0adcbc22dec73e94341a35a
Rian Corcino <riancorci@gmail.com>
2023-04-26 16:07:38 -0700

    modified css and designed blog ui

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,15 @@
 const Navbar = () => {
+
     return (
         <nav className="navbar">
             <h1>The Rian++ Blog</h1>
             <div className="links">
                 <a href="/">Hoom</a>
-                <a href="/create">New Bloog</a>
+                <a href="/create" style={{
+                    color: "white",
+                    backgroundColor: "#f1356d",
+                    borderRadius: "8px"
+                }}>New Bloog</a>
 
             </div>
         </nav>
