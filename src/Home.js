c82d2344da3bcd8ab3614c140cd7c8456ee29939
Rian Corcino <riancorci@gmail.com>
2023-04-27 02:36:40 -0700

    created useFetch hook with useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
 const Home = () => {
+    const {data: blogs, isLoading, Error} = useFetch("http://localhost:8000/blogs")
+
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            {Error && <div> { Error }</div>}
+            {isLoading && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
+
         </div>
     );
 }
